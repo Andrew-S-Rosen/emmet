8e5eb80b6e8930701410d0b28a96f2265ea23aa8
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-12-04 17:29:31 -0800

    check in piezoelectic builder

--- emmet-builders/emmet/builders/materials/piezoelectric.py
+++ emmet-builders/emmet/builders/materials/piezoelectric.py
@@ -14,15 +14,21 @@ from emmet.core.utils import jsanitize
 class PiezoelectricBuilder(Builder):
     def __init__(
         self,
-        materials: Store,
-        tasks: Store,
-        piezoelectric: Store,
+        source_keys: Dict[str, Store],
+        target_keys: Dict[str, Store],
         query: Optional[Dict] = None,
+        chunk_size: int = 300,
+        allow_bson=True,
         **kwargs,
     ):
-        self.materials = materials
-        self.tasks = tasks
-        self.piezoelectric = piezoelectric
+        self.source_keys = source_keys
+        self.target_keys = target_keys
+
+        self.materials = source_keys["materials"]
+        self.tasks = source_keys["tasks"]
+        self.piezoelectric = target_keys["piezoelectric"]
+        self.chunk_size = chunk_size
+        self.allow_bson = allow_bson
         self.query = query or {}
         self.kwargs = kwargs
 
@@ -31,8 +37,9 @@ class PiezoelectricBuilder(Builder):
         self.piezoelectric.key = "material_id"
 
         super().__init__(
-            sources=[materials, tasks],
-            targets=[piezoelectric],
+            sources=[self.materials, self.tasks],
+            targets=[self.piezoelectric],
+            chunk_size=self.chunk_size,
             **kwargs,
         )
 
@@ -113,45 +120,150 @@ class PiezoelectricBuilder(Builder):
 
         mats = [mat for mat in mats_set]
 
-        self.logger.info(
-            "Processing {} materials for piezoelectric data".format(len(mats))
-        )
+        self.logger.info(f"Processing {len(mats)} materials for piezoelectric data")
 
-        self.total = len(mats)
+        return [
+            mat_ids[i : i + self.chunk_size]
+            for i in range(0, len(mat_ids), self.chunk_size)
+        ]
+
+    def get_processed_docs(self, mats):
+        self.materials.connect()
+        self.tasks.connect()
+
+        all_docs = []
 
         for mat in mats:
-            doc = self._get_processed_doc(mat)
+            docs = []
 
-            if doc is not None:
-                yield doc
-            else:
-                pass
+            mat_doc = self.materials.query_one(
+                {self.materials.key: mat},
+                [
+                    self.materials.key,
+                    "structure",
+                    "task_types",
+                    "run_types",
+                    "deprecated_tasks",
+                    "last_updated",
+                ],
+            )
 
-    def process_item(self, item):
-        structure = Structure.from_dict(item["structure"])
-        mpid = item["material_id"]
-        origin_entry = {
-            "name": "piezoelectric",
-            "task_id": item["task_id"],
-            "last_updated": item["task_updated"],
-        }
+            task_types = mat_doc["task_types"].items()
 
-        doc = PiezoelectricDoc.from_ionic_and_electronic(
-            structure=structure,
-            material_id=mpid,
-            origins=[origin_entry],
-            deprecated=False,
-            ionic=item["piezo_ionic"],
-            electronic=item["piezo_static"],
-            last_updated=item["updated_on"],
-        )
+            potential_task_ids = []
 
-        return jsanitize(doc.model_dump(), allow_bson=True)
+            for task_id, task_type in task_types:
+                if task_type == "DFPT Dielectric":
+                    if task_id not in mat_doc["deprecated_tasks"]:
+                        potential_task_ids.append(task_id)
+
+            for task_id in potential_task_ids:
+                task_query = self.tasks.query_one(
+                    properties=[
+                        "last_updated",
+                        "input.is_hubbard",
+                        "orig_inputs.kpoints",
+                        "orig_inputs.poscar.structure",
+                        "input.parameters",
+                        "input.structure",
+                        "output.piezo_tensor",
+                        "output.piezo_ionic_tensor",
+                        "output.bandgap",
+                    ],
+                    criteria={self.tasks.key: str(task_id)},
+                )
+                if task_query["output"]["bandgap"] > 0:
+                    try:
+                        structure = task_query["orig_inputs"]["poscar"]["structure"]
+                    except KeyError:
+                        structure = task_query["input"]["structure"]
+
+                    is_hubbard = task_query["input"]["is_hubbard"]
+
+                    if (
+                        task_query["orig_inputs"]["kpoints"]["generation_style"]
+                        == "Monkhorst"
+                        or task_query["orig_inputs"]["kpoints"]["generation_style"]
+                        == "Gamma"
+                    ):
+                        nkpoints = np.prod(
+                            task_query["orig_inputs"]["kpoints"]["kpoints"][0], axis=0
+                        )
+
+                    else:
+                        nkpoints = task_query["orig_inputs"]["kpoints"]["nkpoints"]
+
+                    lu_dt = mat_doc["last_updated"]
+                    task_updated = task_query["last_updated"]
+
+                    docs.append(
+                        {
+                            "task_id": task_id,
+                            "is_hubbard": int(is_hubbard),
+                            "nkpoints": int(nkpoints),
+                            "piezo_static": task_query["output"]["piezo_tensor"],
+                            "piezo_ionic": task_query["output"]["piezo_ionic_tensor"],
+                            "structure": structure,
+                            "updated_on": lu_dt,
+                            "task_updated": task_updated,
+                            self.materials.key: mat_doc[self.materials.key],
+                        }
+                    )
+
+            if len(docs) > 0:
+                sorted_docs = sorted(
+                    docs,
+                    key=lambda entry: (
+                        entry["is_hubbard"],
+                        entry["nkpoints"],
+                        entry["updated_on"],
+                    ),
+                    reverse=True,
+                )
+                all_docs.append(sorted_docs[0])
+
+        self.materials.close()
+        self.tasks.close()
+
+        return all_docs
+
+    def process_item(self, items):
+        docs = []
+        for item in items:
+            if not item:
+                continue
+
+            structure = Structure.from_dict(item["structure"])
+            mpid = item["material_id"]
+            origin_entry = {
+                "name": "piezoelectric",
+                "task_id": item["task_id"],
+                "last_updated": item["task_updated"],
+            }
+
+            doc = PiezoelectricDoc.from_ionic_and_electronic(
+                structure=structure,
+                material_id=mpid,
+                origins=[origin_entry],
+                deprecated=False,
+                ionic=item["piezo_ionic"],
+                electronic=item["piezo_static"],
+                last_updated=item["updated_on"],
+            )
+
+            docs.append(jsanitize(doc.model_dump(), allow_bson=self.allow_bson))
+
+        return docs
 
     def update_targets(self, items):
         """
         Inserts the new dielectric docs into the dielectric collection
         """
+        if not items:
+            return
+
+        self.piezoelectric.connect()
+
         docs = list(filter(None, items))
 
         if len(docs) > 0:
@@ -160,93 +272,4 @@ class PiezoelectricBuilder(Builder):
         else:
             self.logger.info("No items to update")
 
-    def _get_processed_doc(self, mat):
-        mat_doc = self.materials.query_one(
-            {self.materials.key: mat},
-            [
-                self.materials.key,
-                "structure",
-                "task_types",
-                "run_types",
-                "deprecated_tasks",
-                "last_updated",
-            ],
-        )
-
-        task_types = mat_doc["task_types"].items()
-
-        potential_task_ids = []
-
-        for task_id, task_type in task_types:
-            if task_type == "DFPT Dielectric":
-                if task_id not in mat_doc["deprecated_tasks"]:
-                    potential_task_ids.append(task_id)
-
-        final_docs = []
-
-        for task_id in potential_task_ids:
-            task_query = self.tasks.query_one(
-                properties=[
-                    "last_updated",
-                    "input.is_hubbard",
-                    "orig_inputs.kpoints",
-                    "orig_inputs.poscar.structure",
-                    "input.parameters",
-                    "input.structure",
-                    "output.piezo_tensor",
-                    "output.piezo_ionic_tensor",
-                    "output.bandgap",
-                ],
-                criteria={self.tasks.key: str(task_id)},
-            )
-            if task_query["output"]["bandgap"] > 0:
-                try:
-                    structure = task_query["orig_inputs"]["poscar"]["structure"]
-                except KeyError:
-                    structure = task_query["input"]["structure"]
-
-                is_hubbard = task_query["input"]["is_hubbard"]
-
-                if (
-                    task_query["orig_inputs"]["kpoints"]["generation_style"]
-                    == "Monkhorst"
-                    or task_query["orig_inputs"]["kpoints"]["generation_style"]
-                    == "Gamma"
-                ):
-                    nkpoints = np.prod(
-                        task_query["orig_inputs"]["kpoints"]["kpoints"][0], axis=0
-                    )
-
-                else:
-                    nkpoints = task_query["orig_inputs"]["kpoints"]["nkpoints"]
-
-                lu_dt = mat_doc["last_updated"]
-                task_updated = task_query["last_updated"]
-
-                final_docs.append(
-                    {
-                        "task_id": task_id,
-                        "is_hubbard": int(is_hubbard),
-                        "nkpoints": int(nkpoints),
-                        "piezo_static": task_query["output"]["piezo_tensor"],
-                        "piezo_ionic": task_query["output"]["piezo_ionic_tensor"],
-                        "structure": structure,
-                        "updated_on": lu_dt,
-                        "task_updated": task_updated,
-                        self.materials.key: mat_doc[self.materials.key],
-                    }
-                )
-
-        if len(final_docs) > 0:
-            sorted_final_docs = sorted(
-                final_docs,
-                key=lambda entry: (
-                    entry["is_hubbard"],
-                    entry["nkpoints"],
-                    entry["updated_on"],
-                ),
-                reverse=True,
-            )
-            return sorted_final_docs[0]
-        else:
-            return None
+        self.piezoelectric.close()
